83265cdc3aa1d4a88b9d54b44b007c9e726de947
bcaliva21 <bcaliva21@gmail.com>
2024-12-18 11:19:46 -0600

    feat: backtracking practice

---
+++ leetcode/combo_sum2.rb
@@ -0,0 +1,33 @@
+def combination_sum2(candidates, target)
+  results = []
+  candidates.sort!
+  helper(0, candidates, target, [], results)
+  results.uniq
+end
+
+def helper(index, candidates, target, slate, results)
+  if target == 0
+    results.push(slate.clone)
+    return
+  end
+
+  (index...candidates.length).each do |i|
+    if candidates[i] <= target
+      slate.push(candidates[i])
+      helper(i+1, candidates, target - candidates[i], slate, results)
+      slate.pop()
+    end
+  end
+end
+
+print combination_sum2([10,1,2,7,6,1,5],8)
+
+# Input: candidates = [10,1,2,7,6,1,5], target = 8
+# Input: candidates = [1,1,2,5,6,7,10], target = 8
+# Output: 
+# [
+# [1,1,6],
+# [1,2,5],
+# [1,7],
+# [2,6]
+# ]
